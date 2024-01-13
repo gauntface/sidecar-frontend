126dc50ea8ad15215813cf12b9e1ca811f0c9a59
Matt Gaunt-Seo <hello@gaunt.dev>
2024-01-13 11:18:57 -0800

    Match previous frameowkr

--- src/pages/dashboard/Dashboard.svelte
+++ src/pages/dashboard/Dashboard.svelte
@@ -1,7 +1,14 @@
 <script lang="ts">
   import Layout from "../../ui/layouts/BasicLayout.svelte";
+  import { auth } from "../../logic/auth/firebase";
+
+  let user = auth.user;
+  auth.addEventListener("user-change", () => {
+    user = auth.user;
+  });
 </script>
 
 <Layout>
-  <h1>Dashboard</h1>
+  <h1>Welcome {user?.displayName}</h1>
+  <p>We are happy to see you here</p>
 </Layout>
